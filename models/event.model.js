e89718167124fe271bf87ebafc32558e7d424a1d
raw-brt <robertodzbt@gmail.com>
2019-12-03 11:03:27 +0100

    Event model added

---
+++ models/event.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId;
+    ref: 'User',
+    required: true
+  },
+
+  date: {
+    type: Date,
+    required: true
+  },
+
+  feeling: {
+    type: String,
+    required: true
+  },
+
+  title: {
+    type: String,
+    required: true
+  },
+
+  body: {
+    type: String,
+    maxlength: [280, 'You can write up to 280 characters']
+  },
+}, { timestamps: true });
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
+
