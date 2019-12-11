e19a326e4ce150657effeb903998b0b576a43fda
raw-brt <robertodzbt@gmail.com>
2019-12-11 11:00:40 +0100

    Authentication middleware create. Login, signup and home routes protected

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller')
+const authMiddleware = require('../middlewares/auth.middleware');
 
 // Start route
 router.get('/start', userController.start);
@@ -10,11 +11,11 @@ router.get('/user/new', userController.new);
 router.post('/user/new', userController.create);
 
 // Login routes
-router.get('/user/login', userController.login);
-router.post('/user/login', userController.doLogin);
-router.post('/user/logout', userController.logout);
+router.get('/user/login', userController.login, authMiddleware.isNotAuthenticated);
+router.post('/user/login', authMiddleware.isNotAuthenticated, userController.doLogin);
+router.post('/user/logout', userController.logout, authMiddleware.isNotAuthenticated);
 
 // Home
-router.get('/:userid/home', userController.home);
+router.get('/user/:userid', userController.home, authMiddleware.isAuthenticated);
 
 module.exports = router;
