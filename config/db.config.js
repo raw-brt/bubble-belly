494fac0f1bfa86827aee0d5d230ee03a6b35e4aa
raw-brt <robertodzbt@gmail.com>
2019-12-20 15:27:53 +0100

    DB Config fixed

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://localhost:27017/bubble-belly';
+const MONGODB_URI = 'mongodb://heroku_zvfmxrdq:pr92jk6p0i8hqcj04kmm7hc15a@ds125693.mlab.com:25693/heroku_zvfmxrdq';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => console.log(`Successfully connected to the database ${MONGODB_URI}`))
